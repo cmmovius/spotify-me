b1b579022d39f9982e44e18c1e978aeb379d0240
Christine Movius <christine.movius@gmail.com>
2016-04-12 09:19:32 -0400

    Artist input registers as keyword.

--- js/spotify.js
+++ js/spotify.js
@@ -1,9 +1,18 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
-
+$(document).ready( function() {
+  $("#submit").on("click", function(event){
+    event.preventDefault();
+    console.log("clicked!");
+    searchByArtist($("input[name='keyword']").val());
+  });
+});
 
 function searchByArtist(keyword) {
+  console.log("Search Artist");
+  console.log(keyword);
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+  console.log(url);
 }
 
 
